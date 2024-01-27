e22f7b5b6449564e6371de4e416eda9c73f918b1
LuloDev <ezequielcruz018@gmail.com>
2024-01-27 11:21:07 -0400

    modified:   Harmony.py 	modified:   PyWhatKit_DB.txt 	deleted:    recface.py

--- Harmony.py
+++ Harmony.py
@@ -1,34 +1,32 @@
 import pyjokes
 import pyttsx3
 import pywhatkit
-import wikipedia, webbrowser
+import wikipedia
+import webbrowser
 import datetime
 import time
 import speech_recognition as sr
-import yfinance as yf
 import subprocess
 import pyautogui
 from pathlib import Path
 
-usuario = "Ezequiel" 
 voz = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Speech\\Voices\\Tokens\\TTS_MS_ES-MX_SABINA_11.0"
-
 
 
 def transformar_audio_texto():
     r = sr.Recognizer()
     with sr.Microphone() as origen:
         r.pause_threshold = 0.8
-        print("ya puedes hablar")  
+        print("ya puedes hablar")
         audio = r.listen(origen)
 
         try:
             pedido = r.recognize_google(audio, language="es-mx")
             print("Dijiste: " + pedido)
             return pedido
-        
+
         except sr.UnknownValueError:
-            print("Uis... No entendi")
+            print("Uis... No entendí")
             hablar("Disculpa, no te he entendido")
             return "sigo esperando"
 
@@ -37,8 +35,9 @@ def transformar_audio_texto():
             return "sigo esperando"
 
         except:
-            print("Uis... Algo salio mal")
+            print("Uis... Algo salió mal")
             return "sigo esperando"
+
 
 def hablar(mensaje):
     engine = pyttsx3.init()
@@ -46,8 +45,8 @@ def hablar(mensaje):
     engine.say(mensaje)
     engine.runAndWait()
 
-def pedir_dia():
 
+def pedir_dia():
     dia = datetime.datetime.today()
     print(dia)
 
@@ -55,28 +54,33 @@ def pedir_dia():
     mes_actual = dia.month
     print(dia_semana)
 
-    calendario = {0: "Lunes",
-                  1: "Martes",
-                  2: "Miércoles",
-                  3: "Jueves",
-                  4: "Viernes",
-                  5: "Sábado",
-                  6: "Domingo"}
+    calendario = {
+        0: "Lunes",
+        1: "Martes",
+        2: "Miércoles",
+        3: "Jueves",
+        4: "Viernes",
+        5: "Sábado",
+        6: "Domingo",
+    }
 
-    meses = {1: "Enero",
-             2: "Febrero",
-             3: "Marzo",
-             4: "Abril",
-             5: "Mayo",
-             6: "Junio",
-             7: "Julio",
-             8: "Agosto",
-             9: "Septiembre",
-             10: "Octubre",
-             11: "Noviembre",
-             12: "Diciembre"}
+    meses = {
+        1: "Enero",
+        2: "Febrero",
+        3: "Marzo",
+        4: "Abril",
+        5: "Mayo",
+        6: "Junio",
+        7: "Julio",
+        8: "Agosto",
+        9: "Septiembre",
+        10: "Octubre",
+        11: "Noviembre",
+        12: "Diciembre",
+    }
 
     hablar(f"Hoy es {calendario[dia_semana]} {dia.day} de {meses[mes_actual]}")
+
 
 def pedir_hora():
     hora = datetime.datetime.now()
@@ -84,22 +88,31 @@ def pedir_hora():
     print(hora)
     hablar(hora)
 
-def saludo_inicial():
+
+def pedir_informacion():
+    hablar("Hola, ¿Cómo te llamas?")
+    nombre = transformar_audio_texto()
+    hablar(f"¡Hola {nombre}! ¿Cómo te sientes hoy?")
+    animo = transformar_audio_texto()
+    return nombre, animo
+
+
+def saludo_inicial(nombre, animo):
     hora_actual = datetime.datetime.now()
 
     if 6 <= hora_actual.hour < 12:
-        momento = f"Buenos días{usuario}"
+        momento = f"Buenos días {nombre}"
     elif 12 <= hora_actual.hour < 18:
-        momento = f"Buenas Tardes{usuario}"
+        momento = f"Buenas Tardes {nombre}"
     else:
-        momento = f"Buenas noches{usuario}"
+        momento = f"Buenas noches {nombre}"
 
-    hablar(f"{momento}, Soy harmony, tu asistente virtual, veo que hoy estas feliz, ")
+    hablar(f"{momento}, Soy harmony, tu asistente virtual, veo que hoy estás {animo}, ")
     mensaje_opciones = "Cuentas con un menú interactivo. Elige a gusto"
     hablar(mensaje_opciones)
 
-def pedir_aplicacion():
 
+def pedir_aplicacion():
     lista_rutas = []
     lista_nombres = []
     ruta = Path("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs")
@@ -108,106 +121,99 @@ def pedir_aplicacion():
         lista_rutas.append(app)
         lista_nombres.append(app.stem.lower())
 
-    return lista_nombres, print( lista_rutas)
+    return lista_nombres, lista_rutas
+
 
 def abrir_aplicacion(pedido, nombres, rutas):
+    try:
+        indice_ruta_seleccionada = nombres.index(pedido)
+        aplicacion = rutas[indice_ruta_seleccionada]
+        subprocess.Popen(fr'"{aplicacion}"', shell=True)
+    except ValueError:
+        hablar("No he encontrado esa aplicación. Inténtalo otra vez")
 
-    correcto = False
-    indice_ruta_seleccionada = nombres.index(pedido)
-
-    while not correcto:
-
-        if pedido in nombres:
-
-            aplicacion = rutas[indice_ruta_seleccionada]
-            subprocess.Popen(fr'{aplicacion}', shell=True)
-            correcto = True
-
-        else:
-            hablar('No he encontrado esa aplicación. Inténtalo otra vez')
 
 def abrir_navegador(pedido):
     subprocess.Popen(fr'start opera {pedido}', shell=True)
 
+
 def pedir_cosas():
-    saludo_inicial()
+    nombre, animo = pedir_informacion()
+    saludo_inicial(nombre, animo)
 
     comenzar = True
     while comenzar:
-
         print("\nSelecciona una Opcion:")
         print("1. Buscar en Internet")
-        print("2. Abrir aplicacion")
+        print("2. Abrir aplicación")
         print("3. Buscar Noticias")
         print("4. Pedir la fecha")
         print("5. Pedir la hora")
-        print("6. Buscar en wikipedia")
-        print("7. Reproducir musica")
+        print("6. Buscar en Wikipedia")
+        print("7. Reproducir música")
         print("8. Contar un chiste")
         print("9. Enviar un mensaje")
         print("10. Salir")
 
-        pedido = input("Opcion:")
-        hablar(input)
+        pedido = transformar_audio_texto()
 
-        if pedido == "1":
+        if "uno" in pedido or "internet" in pedido:
             hablar("Ya mismo estoy en eso")
             pedido = pedido.replace("busca en internet", "")
             pywhatkit.search(pedido)
             hablar("Esto es lo que he encontrado")
             continue
 
-        elif pedido == "2":
+        elif "dos" in pedido or "aplicación" in pedido:
             nombres, rutas = pedir_aplicacion()
-            hablar("Muy bien, qué aplicación deseas abrir?")
+            hablar("Muy bien, ¿qué aplicación deseas abrir?")
             pedido = transformar_audio_texto().lower()
             hablar(f"Abriendo {pedido}")
             abrir_aplicacion(pedido, nombres, rutas)
-            continue   
-        
+            continue
 
-        elif pedido == "3":
+        elif "noticia" in pedido or "tres" in pedido:
             webbrowser.open("https://www.diariolibre.com")
-        
-        elif pedido == "4":
+
+        elif "cuatro" in pedido or "fecha" in pedido:
             pedir_dia()
             continue
 
-        elif pedido == "5":
+        elif "cinco" in pedido or "hora" in pedido:
             pedir_hora()
             continue
 
-        elif pedido == "6":
-            hablar("Buscando en wikipedia")
-            pedido = pedido.replace("wikipedia", '')
+        elif "seis" in pedido or "wikipedia" in pedido:
+            hablar("Buscando en Wikipedia")
+            pedido = pedido.replace("wikipedia", "")
             wikipedia.set_lang("es")
             resultado = wikipedia.summary(pedido, sentences=1)
-            hablar("Wikipedia dice los siguiente:")
+            hablar("Wikipedia dice lo siguiente:")
             hablar(resultado)
             continue
 
-        elif pedido == "7":
+        elif "siete" in pedido or "reproduce" in pedido:
             pywhatkit.playonyt(pedido)
             continue
 
-        elif pedido == "8":
+        elif "ocho" in pedido or "chiste" in pedido:
             hablar(pyjokes.get_joke("es"))
             continue
 
-
-        elif pedido == "9":
+        elif "nueve" in pedido or "mensaje" in pedido:
             hablar("Enviando el mensaje")
-            mensaje = pedido.split("diga")[-1].strip()
+            mensaje = transformar_audio_texto().split("diga")[-1].strip()
             pywhatkit.sendwhatmsg_instantly("+18299673641", mensaje)
             time.sleep(5)
             pyautogui.press("enter")
             continue
 
-        elif pedido == "10":
-            hablar("claro, ya sabes donde encontrarme")
+        elif "diez" in pedido or "salir" in pedido:
+            hablar("Claro, ya sabes donde encontrarme")
             comenzar = False
-    
+
         else:
-            hablar("Lo siento, no entiendo esa opcion.Por favor, selecciona una opción válida.")
+            hablar("Lo siento, no entiendo esa opción. Por favor, selecciona una opción válida.")
+
 
 pedir_cosas()
