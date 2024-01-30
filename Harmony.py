efee87b55b61208fd7d26973b43c0ba76c900da9
LuloDev <ezequielcruz018@gmail.com>
2024-01-30 12:25:01 -0400

    modified:   Harmony.py

--- Harmony.py
+++ Harmony.py
@@ -8,8 +8,11 @@ import time
 import speech_recognition as sr
 import subprocess
 import pyautogui
+import threading
+import openai
 from pathlib import Path
 
+openai.api_key = "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"
 voz = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Speech\\Voices\\Tokens\\TTS_MS_ES-MX_SABINA_11.0"
 
 
@@ -38,13 +41,34 @@ def transformar_audio_texto():
             print("Uis... Algo salió mal")
             return "sigo esperando"
 
+def obtener_respuesta(pregunta):
+    try:
+        respuesta_creada = openai.completions.create(
+            engine = "text-davinci-002",
+            prompt=pregunta,
+            max_tokens=150
+            
+        )
+        return respuesta_creada.choices[0].text.strip()
+    
+    except  Exception as e:
+        print(f"Error al obtener respuesta{e}")
+        return "Lo siento, hubo un error al procesar tu solicitud"
+
+def buscar_en_internet(pedido):
+    hablar("ya mismo estoy en eso")
+    pedido = pedido.replace("buscar en internet")
+    threading.Thread(target=buscar_en_internet_thread, args=(pedido,)).start()
+
+def buscar_en_internet_thread(pedido):
+    pywhatkit.search(pedido)
+    hablar("Esto es lo que he encontrado")
 
 def hablar(mensaje):
     engine = pyttsx3.init()
     engine.setProperty("voice", voz)
     engine.say(mensaje)
     engine.runAndWait()
-
 
 def pedir_dia():
     dia = datetime.datetime.today()
@@ -81,13 +105,11 @@ def pedir_dia():
 
     hablar(f"Hoy es {calendario[dia_semana]} {dia.day} de {meses[mes_actual]}")
 
-
 def pedir_hora():
     hora = datetime.datetime.now()
     hora = f" En este momento son las {hora.hour} horas con {hora.minute} minutos"
     print(hora)
     hablar(hora)
-
 
 def pedir_informacion():
     hablar("Hola, ¿Cómo te llamas?")
@@ -95,7 +117,6 @@ def pedir_informacion():
     hablar(f"¡Hola {nombre}! ¿Cómo te sientes hoy?")
     animo = transformar_audio_texto()
     return nombre, animo
-
 
 def saludo_inicial(nombre, animo):
     hora_actual = datetime.datetime.now()
@@ -118,7 +139,6 @@ def saludo_inicial(nombre, animo):
     else:
         hablar(f"Desplegare el menu de opciones.")
 
-
 def pedir_aplicacion():
     lista_rutas = []
     lista_nombres = []
@@ -130,19 +150,19 @@ def pedir_aplicacion():
 
     return lista_nombres, lista_rutas
 
-
 def abrir_aplicacion(pedido, nombres, rutas):
     try:
         indice_ruta_seleccionada = nombres.index(pedido)
         aplicacion = rutas[indice_ruta_seleccionada]
-        subprocess.Popen(fr'"{aplicacion}"', shell=True)
+        threading.Thread(target=abrir_aplicacion_thread, args=(aplicacion,)).start()
     except ValueError:
         hablar("No he encontrado esa aplicación. Inténtalo otra vez")
 
+def abrir_aplicacion_thread(aplicacion):
+    subprocess.Popen(fr'"{aplicacion}"', shell=True)
 
 def abrir_navegador(pedido):
     subprocess.Popen(fr'start opera {pedido}', shell=True)
-
 
 def pedir_cosas():
     nombre, animo = pedir_informacion()
@@ -164,14 +184,11 @@ def pedir_cosas():
 
         pedido = transformar_audio_texto()
 
-        if "uno" in pedido or "internet" in pedido:
-            hablar("Ya mismo estoy en eso")
-            pedido = pedido.replace("busca en internet", "")
-            pywhatkit.search(pedido)
-            hablar("Esto es lo que he encontrado")
-            continue
+        if "internet" in pedido:
+           buscar_en_internet(pedido)
+           continue
 
-        elif "dos" in pedido or "aplicación" in pedido:
+        elif "aplicación" in pedido:
             nombres, rutas = pedir_aplicacion()
             hablar("Muy bien, ¿qué aplicación deseas abrir?")
             pedido = transformar_audio_texto().lower()
@@ -179,18 +196,18 @@ def pedir_cosas():
             abrir_aplicacion(pedido, nombres, rutas)
             continue
 
-        elif "noticia" in pedido or "tres" in pedido:
+        elif "noticias" in pedido:
             webbrowser.open("https://www.diariolibre.com")
 
-        elif "cuatro" in pedido or "fecha" in pedido:
+        elif "fecha" in pedido:
             pedir_dia()
             continue
 
-        elif "cinco" in pedido or "hora" in pedido:
+        elif "hora" in pedido:
             pedir_hora()
             continue
 
-        elif "seis" in pedido or "wikipedia" in pedido:
+        elif "wikipedia" in pedido:
             hablar("Buscando en Wikipedia")
             pedido = pedido.replace("wikipedia", "")
             wikipedia.set_lang("es")
@@ -199,15 +216,15 @@ def pedir_cosas():
             hablar(resultado)
             continue
 
-        elif "siete" in pedido or "reproducir musica" in pedido:
+        elif "reproduce" in pedido:
             pywhatkit.playonyt(pedido)
             continue
 
-        elif "ocho" in pedido or "chiste" in pedido:
+        elif "chiste" in pedido:
             hablar(pyjokes.get_joke("es"))
             continue
 
-        elif "nueve" in pedido or "mensaje" in pedido:
+        elif "mensaje" in pedido:
             hablar("Enviando el mensaje")
             mensaje = transformar_audio_texto().split("diga")[-1].strip()
             pywhatkit.sendwhatmsg_instantly("+18299673641", mensaje)
@@ -215,12 +232,11 @@ def pedir_cosas():
             pyautogui.press("enter")
             continue
 
-        elif "diez" in pedido or "salir" in pedido:
+        elif "Eso es todo" in pedido:
             hablar("Claro, ya sabes donde encontrarme")
             comenzar = False
 
         else:
             hablar("Lo siento, no entiendo esa opción. Por favor, selecciona una opción válida.")
-
 
 pedir_cosas()
